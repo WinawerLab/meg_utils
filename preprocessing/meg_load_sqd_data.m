9e96e25fa3cb380da95981c4eebff8f8cc7a1a18
Jonathan Winawer <jonathan.winawer@nyu.edu>
2014-09-19 14:54:38 -0400

    new file to load quid data and concatenate data sets if there is more than one

---
+++ preprocessing/meg_load_sqd_data.m
@@ -0,0 +1,33 @@
+function [ts, meg_files] = meg_load_sqd_data(data_pth, fname_str)
+% This function loads the SQD MEG files and concatenates them, if there is
+% more than one.
+%
+% Dependencies: fieldtrip
+%
+% INPUTS:
+% data_pth      Absolute data path to the session folder. This folder must
+%               contain a raw and behavior folder for the sqd and mat files.
+% fname_str     A character array contained in the name of data files
+%
+% OUTPUTS:
+% ts:           Timeseries of MEG experiment (timepoints x channels)
+% meg_files:    MEG file names, needed to read header for topoplot
+
+% If user doesn't specify fname_str, look for any sqd file
+if ~exist('fname_str', 'var'), fname_str = '*'; end
+
+% Find MEG and stimulus files:
+meg_files = dir(fullfile(data_pth, sprintf('%s.sqd', fname_str)));
+num_files = numel(meg_files);
+
+% Load the MEG data for all possible blocks
+tmp = cell(1, num_files);
+for ii = 1:num_files
+    tmp{ii} = sqdread(fullfile(data_pth, meg_files(ii).name));
+end
+
+% Concatenate timeseries if there are more blocks of MEG data
+% TODO: Make this a general statement
+ts = vertcat(tmp{:});
+
+return
