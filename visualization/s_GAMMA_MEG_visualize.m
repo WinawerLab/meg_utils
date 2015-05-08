687aec3e6c57f456707645b4eb1d78c699db4e47
Nicholas Chua <jonathan.winawer@nyu.edu>
2015-05-08 18:02:46 -0400

    code to save SNR meshes

--- visualization/s_GAMMA_MEG_visualize.m
+++ visualization/s_GAMMA_MEG_visualize.m
@@ -5,6 +5,7 @@
 project_pth                   = '/Volumes/server/Projects/MEG/Gamma/Data';
 data_pth                      = '*_Gamma_*subj*';
 
+
 data_channels                 = 1:157;
 num_channels                  = length(data_channels);
 fs                            = 1000;
@@ -31,6 +32,10 @@ condition_names               = {   ...
 
 %% Loop over data sets
 for subject_num = which_data_to_visualize
+    %% Save path
+    
+    save_pth = fullfile(project_pth,'/Images',subj_pths{subject_num}) ; 
+    
     %% Load Data
     load_pth    = fullfile(project_pth, subj_pths{subject_num}, 'processed');
     d           =  dir(fullfile(load_pth, '*boot*'));
@@ -46,10 +51,6 @@ for subject_num = which_data_to_visualize
     end
     
     contrasts = [...
-        1 1 1 1 0 0 0 0 0 -4; ...    % noise - baseline
-        0 0 0 0 1 1 1 1 0 -4; ...    % gratings - baseline
-        1 1 1 1 -1 -1 -1 -1 0 0; ... % noise - gratings
-        -1 -1 -1 -1 1 1 1 1 0 0; ... % gratings - noise
         1 0 0 0 0 0 0 0 0 -1; ...    % white noise - baseline
         0 1 0 0 0 0 0 0 0 -1; ...    % binarized white noise - baseline
         0 0 1 0 0 0 0 0 0 -1; ...    % pink noise - baseline
@@ -58,13 +59,13 @@ for subject_num = which_data_to_visualize
         0 0 0 0 0 1 0 0 0 -1; ...    % 0.73cpd gratings - baseline
         0 0 0 0 0 0 1 0 0 -1; ...    % 1.46cpd gratings - baseline
         0 0 0 0 0 0 0 1 0 -1; ...    % 2.90cpd gratings - baseline
-        0 0 0 0 0 0 0 0 1 -1];       % plaid - baseline
-    
+        0 0 0 0 0 0 0 0 1 -1; ...    % plaid - baseline
+        1 1 1 1 0 0 0 0 0 -4; ...    % noise - baseline
+        0 0 0 0 1 1 1 1 0 -4; ...    % gratings - baseline
+        1 1 1 1 -1 -1 -1 -1 0 0; ... % noise - gratings
+        -1 -1 -1 -1 1 1 1 1 0 0; ... % gratings - noise
+    ];
     contrastnames = {
-        'noise - baseline'...
-        'gratings - baseline'...
-        'noise - gratings'...
-        'gratings - noise'...
         'white noise - baseline'...
         'binwn - baseline'...
         'pink noise - baseline'...
@@ -73,7 +74,12 @@ for subject_num = which_data_to_visualize
         '0.73cpd gratings - baseline'...
         '1.46cpd gratings - baseline'...
         '2.90cpd gratings - baseline'...
-        'plaid - baseline'};
+        'plaid - baseline' ...
+        'noise - baseline'...
+        'gratings - baseline'...
+        'noise - gratings'...
+        'gratings - noise'...        
+        };
     
     
     % ensure each condition is weighted proportionatly in each contrast
@@ -109,15 +115,32 @@ for subject_num = which_data_to_visualize
     %% SNR Mesh
     threshold = 0;%3;
     % gaussian weight for each stimuli
-    fH = figure(998); clf, set(fH, 'name', 'Gaussian weight')
-    for c = 1:12
-        subplot(3,4,c)
+    fH = figure(1005); clf, set(fH, 'name', 'Gaussian weight' )
+    %for c = 1:12
+    for c = 1:9
+        subplot(3,3,c)
         data_to_plot = snr_w_gauss(:,c)';
         data_to_plot(abs(data_to_plot) < threshold) = 0;
         ft_plotOnMesh(data_to_plot, contrastnames{c});
         set(gca, 'CLim', [-1 1]* 10)
     end
     
+    hgexport(fH, fullfile(save_pth,'Mesh_gamma_SNR'));
+
+    %% SNR Mesh
+    threshold = 0;%3;
+    % gaussian weight for each stimuli
+    fH = figure(455); clf, set(fH, 'name', 'Gaussian weight')
+    %for c = 1:12
+    for c = 10:13
+        subplot(2,2,c-9)
+        data_to_plot = snr_w_gauss(:,c)';
+        data_to_plot(abs(data_to_plot) < threshold) = 0;
+        ft_plotOnMesh(data_to_plot, contrastnames{c});
+        set(gca, 'CLim', [-1 1]* 10)
+    end
+    
+    hgexport(fH, fullfile(save_pth,'Mesh_broadband_SNR'));
     %% Noise (Gamma SNR - baseline)
     
     threshold = 1;
@@ -199,90 +222,7 @@ for subject_num = which_data_to_visualize
     
     hgexport(fH, fullfile(project_pth,'figure_bbpower_noise_s1.eps'));
     
-    %% Plot Gaussian fits
-    
-    % include spectral_data_boots to the HPC save script to produce
-    % spectral_data_mean
-    
-    line_width = 2; % line width for
-    for chan = data_channels
-        fH = figure(10); clf, set(gcf, 'Position', [100 100 800 800], 'Color', 'w')
-        
-        data_base = spectral_data_mean(:,num_conditions,chan);
-        for cond = 1:num_conditions-1
-            data_fit = spectral_data_mean(:,cond,chan,:);
-            subplot(3,3,cond)
-            % plot fit
-            plot(f,10.^(fit_f2_mn(cond,:, chan)),'Color','g','LineWidth',line_width)
-            hold on;
-            
-            % plot baseline data
-            plot(f(f_sel),data_base(f_sel),'k--','LineWidth',line_width)
-            % plot stimulus data
-            plot(f(f_sel),data_fit(f_sel),'b-','LineWidth',line_width)
-            
-            set(gca, 'XScale', 'log', 'YScale', 'log', ...
-                'XLim', [min(f_use4fit) max(f_use4fit)], ...
-                'YLim', 10.^[0.3 1.5] )
-            title(sprintf('Subject %d, Channel %d, %s', subject_num, chan, condition_names{cond}))
-        end
-        if save_images,
-            
-            hgexport(fH, fullfile(save_pth, sprintf('Spectra_Chan%03d.eps', chan)));
-        else
-            waitforbuttonpress
-        end
-    end
-    
-    %% Plot spectra
-    fH = figure(2); clf,  set(gcf, 'Position', [100 100 1000 400], 'Color', 'w')
-    
-    colors = zeros(num_conditions,3);
-    colors(1:4,:) = ([0 1 0]'*[.4 .6 .8 1])';
-    colors(5:9,:) = ([1 0 0]'*[.4 .55 .7 .85 1])'; %hsv(5);
-    colors(10,:)  = [0 0 0];
-    yl            =  10.^([0.5 1.6]);
-    xl            = [30 200];
-    lw = 4;
-    for chan = data_channels
-        
-        % Plot Data -----------------------------------------------------------
-        subplot(1,3,1); cla; hold on
-        for cond = 1:num_conditions
-            plot(f(f_sel), squeeze(spectral_data_mean(f_sel,cond,chan)), 'Color', colors(cond,:), 'LineWidth', lw);
-        end
-        set(gca, 'YScale', 'log','XScale', 'log', 'YLim', yl, 'XLim', xl,  ...
-            'Color', [1 1 1], 'XGrid', 'on', ...
-            'XTick', [10 60 100 200]);
-        title(sprintf('Data from channel %d, subject %d', chan, subject_num))
-        
-        % Plot Fits -----------------------------------------------------------
-        subplot(1,3,2); cla; hold on
-        for cond = 1:num_conditions
-            plot(f, squeeze(10.^(fit_f2_mn(cond,:, chan))), 'Color', colors(cond,:), 'LineWidth', lw);
-        end
-        set(gca, 'YScale', 'log','XScale', 'log', 'YLim', yl, 'XLim', xl, ...
-            'Color', [1 1 1], 'XGrid', 'on', ...
-            'XTick', [10 60 100 200]);
-        title(sprintf('Fits to channel %d, subject %d', chan, subject_num))
-        
-        % Legend  -----------------------------------------------------------
-        subplot(1,3,3); cla
-        
-        set(gca, 'ColorOrder', colors); hold all
-        plot(zeros(10,10), zeros(10,10), '-')
-        box off; axis off;
-        legend(condition_names)
-        drawnow;
-        if save_images
-            hgexport(fH, fullfile(save_pth, sprintf('Spectral_fits_Chan%03d.eps', chan)));
-        else
-            waitforbuttonpress
-        end
-        
-    end
-    %axis tight
-    
+   
     %% Meshes of Gaussian/Broadband Weight
     
     % TODO: threshold maps by significance: w_gauss_mn./w_gauss_sd>2
@@ -291,7 +231,7 @@ for subject_num = which_data_to_visualize
     fH = figure(998); clf, set(fH, 'name', 'Gaussian weight')
     for cond = 1:9
         subplot(3,3,cond)
-        ft_plotOnMesh(w_gauss_mn(:,cond)', condition_names{cond});
+        ft_plotOnMesh(w_gauss_mn(:,cond)' - w_gauss_mn(:,num_conditions)', condition_names{cond});
         set(gca, 'CLim', [0 .2])
     end
     
