0eaae5790056518f355f05a13598b0b33cba581a
Jonathan Winawer <jonathan.winawer@nyu.edu>
2015-06-18 16:18:59 -0400

    meg gamma updates

--- experiments/gamma_meg/denoiseGamma.m
+++ experiments/gamma_meg/denoiseGamma.m
@@ -1,12 +1,17 @@
-function denoiseGamma(subjects)
+function denoiseGamma(subject)
 %
-% denoiseGamma(subjects, howToDenoise)
+% denoiseGamma(subject)
 %
 % INPUTS:
-% subjects  : Number of subjects one would like to denoise
+% subjects  : Subject number to denoise
 %
-% DESCRIPTION: Wrapper function to denoise multiple MEG visual gamma data
-% sets. The results of the denoising are written to file.
+% DESCRIPTION: Wrapper function to denoise  MEG visual gamma data sets.
+
+
+% TODO:
+%      1. function should return something
+%      2. write a preprocessing function
+%      3. add environmental denoising
 
 project_pth                     = '/Volumes/server/Projects/MEG/Gamma/Data';
 
@@ -21,13 +26,49 @@ bad_epoch_threshold    = 0.2;
 data_channels          = 1:157;
 environmental_channels = 158:160;
 trigger_channels       = 161:164;
-epoch_start_end        = [0.050 1.049];% start and end of epoch, relative to trigger, in seconds
+epoch_start_end        = [0.050 1.049-.5];% start and end of epoch, relative to trigger, in seconds
 fs                     = 1000;        % sample rate
 intertrial_trigger_num = 11;          % the MEG trigger value that corresponds to the intertrial interval
+blank_condition        = 10;          % the MEG trigger value that corresponds to trials with zero contrast
 verbose                = true;
 
+
+
+d = dir(fullfile(project_pth, data_pth));
+subj_pths = struct2cell(d);
+subj_pths = subj_pths(1,:);
+
+% Load and denoise data, one subject at a time
+
+%% Load data (SLOW)
+raw_ts = meg_load_sqd_data(fullfile(project_pth, subj_pths{subject}, 'raw'), '*Gamma*');
+
+%% Extract triggers
+trigger = meg_fix_triggers(raw_ts(:,trigger_channels));
+
+%% Make epochs
+[ts, conditions]  = meg_make_epochs(raw_ts, trigger, epoch_start_end, fs);
+% remove intertrial intervals
+iti               = conditions == intertrial_trigger_num;
+ts                = ts(:,~iti, :);
+conditions        = conditions(~iti);
+conditions(blank_condition) = 0;
+
+%% Remove bad epochs and bad channels
+
+[ts(:,:,data_channels), bad_channels, bad_epochs] = meg_preprocess_data(ts(:,:,data_channels), ...
+    var_threshold, bad_channel_threshold, bad_epoch_threshold, 'meg160xyz', verbose);
+
+ts = ts(:,~bad_epochs,~bad_channels);
+
+conditions = conditions(~bad_epochs);
+
+design_mtrx = conditions2design(conditions);
+
+%% Denoise the data
+
 % denoise parameters (see denoisedata.m)
-opt.pcchoose        = 1.05;   % denoise with exactly 10 PCs for broadband
+opt.pcchoose        = 1.05;
 opt.npcs2try        = 10;
 opt.resampling      = {'xval','xval'}; % could be {'boot' 'boot'};
 opt.pcselmethod     = 'r2';            % could be 'snr';
@@ -39,67 +80,29 @@ opt.pcselmethod     = 'r2';            % could be 'snr';
 %        (b) We want to keep these frequencies and filter out the rest:
 %               f_use4fit = f((f>=35 & f < 40) |(f > 40 & f <= 57) | ...
 %                   (f>=65 & f <= 115) | (f>=126 & f <= 175) | (f>=186 & f <= 200));
-%      
-opt.preprocessfun   = @hpf;  % preprocess data with a high pass filter for broadband analysis
-
+%
+t                   = epoch_start_end(1):1/fs:epoch_start_end(2);
+f                   = (0:length(t)-1)*fs/length(t);
+keep_timepts        = t(t>.250);
+keep_frequencies    = f((f>=35 & f < 40) |(f > 40 & f <= 57) | ...
+                   (f>=65 & f <= 115) | (f>=126 & f <= 175) | (f>=186 & f <= 200));
+opt.preprocessfun   = @(x)gammapreprocess(x, keep_timepts, keep_frequencies);  % preprocess data by clipping 1st
 
 % 2. We need a new evoked function that extracts the evoked amplitude
-evokedfun             = @(x)getevoked(data,opts); % function handle to determine noise pool
+evokedfun           = @(x)getevoked(x, fs, design_mtrx, [-30 30]); % function handle to determine noise pool
 
 % 3. We need a new eval function that extracts gamma and broadband
+f                     = (0:size(ts,1)-1)/max(t);
+freq                  = megGetSLandABfrequencies(f,[],60);
 evalfun               = @(x)getbroadband(x,freq);  % function handle to compuite broadband
 
 
-d = dir(fullfile(project_pth, data_pth));
-subj_pths = struct2cell(d);
-subj_pths = subj_pths(1,:);
+% Permute sensorData for denoising
+ts = permute(ts, [3 1 2]);
 
-subjects = 5;
+% Denoise for broadband analysis
+[results,evalout,denoisedspec,denoisedts] = denoisedata(design_mtrx,ts,evokedfun,evalfun,opt);
 
-% Load and denoise data, one subject at a time
-for which_subject = subjects
-   
-    %% Load data (SLOW)
-    raw_ts = meg_load_sqd_data(fullfile(project_pth, subj_pths{which_subject}, 'raw'), '*Gamma*');
-    
-    %% Extract triggers
-    trigger = meg_fix_triggers(raw_ts(:,trigger_channels));
-    
-    %% Make epochs
-    [ts, conditions]  = meg_make_epochs(raw_ts, trigger, epoch_start_end, fs);
-    % remove intertrial intervals
-    iti               = conditions == intertrial_trigger_num;
-    ts                = ts(:,~iti, :);
-    conditions        = conditions(~iti);
-    
-    %% Remove bad epochs    
-    
-    [ts(:,:,data_channels), bad_channels, bad_epochs] = meg_preprocess_data(ts(:,:,data_channels), ...
-        var_threshold, bad_channel_threshold, bad_epoch_threshold, 'meg160xyz', verbose);
-    
-    ts = ts(:,~bad_epochs,data_channels);
-    ts(:,:, bad_epochs) = NaN;
-    
-    conditions = conditions(~bad_epochs);
-    
-    %% Make the design matrix
-    num_non_blank = length(unique(conditions))-1;
-    design = zeros(length(conditions), num_non_blank);
-    for ii = 1:length(conditions)
-        if conditions(ii) <= max(num_non_blank)
-            design(ii,conditions(ii)) = 1;
-        end
-    end
-    
-    
-    
-    %% Denoise the data
-    % Permute sensorData for denoising
-    ts = permute(ts, [3 1 2]);
-    
-    % Denoise for broadband analysis
-    [results,evalout,denoisedspec,denoisedts] = denoisedata(design,ts,evokedfun,evalfun,opt);
-        
-        
 
-end
+
+
