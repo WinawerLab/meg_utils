f88e35bea7e0cd11facd6fcc3890e15df86ef99f
Jonathan Winawer <jonathan.winawer@nyu.edu>
2015-10-06 18:36:46 -0400

    minor updates to gamma experiments

--- experiments/gamma_meg/s_plot_modelfit_before_after_denoising.m
+++ experiments/gamma_meg/s_plot_modelfit_before_after_denoising.m
@@ -1,4 +1,4 @@
-% Visualizes summary statistics computed in s_GAMMA_MEG_analysis.m
+% Visualize spectra and gamma + bb model fits
 
 project_pth                   = '/Volumes/server/Projects/MEG/Gamma/Data';
 data_pth                      = '*_Gamma_*subj*';
@@ -6,8 +6,11 @@ data_pth                      = '*_Gamma_*subj*';
 
 fs                            = 1000;
 intertrial_trigger_num        = 10;
-which_data_to_visualize       = 15;
+which_data_to_visualize       = 6;
 save_images                   = true;
+
+before_dataset                = 2;
+after_dataset                 = 4;
 
 % meg_add_fieldtrip_paths('/Volumes/server/Projects/MEG/code/fieldtrip',{'yokogawa', 'sqdproject'})
 
@@ -15,17 +18,7 @@ d = dir(fullfile(project_pth, data_pth));
 subj_pths = struct2cell(d);
 subj_pths = subj_pths(1,:);
 
-condition_names               = {   ...
-    'White Noise' ...
-    'Binarized White Noise' ...
-    'Binarized Pink Noise' ...
-    'Binarized Brown Noise' ...
-    'Gratings (0.36 cpd)' ...
-    'Gratings (0.73 cpd)' ...
-    'Gratings (1.45 cpd)' ...
-    'Gratings (2.90 cpd)' ...
-    'Plaid'...
-    'Blank'};
+
 
 rgb_pink  = [228,43,145]/255;
 rgb_green   = [141,200,108]/255;
@@ -46,21 +39,35 @@ colorRGB_noise  = varysat(rgb_green,satValues);
 cmap = copper(9);
 for subject_num = which_data_to_visualize
     
+    condition_names  = gamma_get_condition_names(subject_num);
+
     load_pth    = fullfile(project_pth, subj_pths{subject_num}, 'processed');
-    datasets    =  dir(fullfile(load_pth, '*boot*'));
-    before      = load(fullfile(load_pth, datasets(2).name));
-    if subject_num < 6
-        after = load(fullfile(load_pth, datasets(3).name));
-    else 
-        after = load(fullfile(load_pth, datasets(2).name));
+    datasets    = dir(fullfile(load_pth, '*boot*'));
+    
+    for ii = 1:numel(datasets)
+        fprintf('%d: %s\n', ii, datasets(ii).name)
     end
+    before_dataset = input('Before data set number?');
+    after_dataset = input('After data set number?');
+    
+    before  = load(fullfile(load_pth, datasets(before_dataset).name));   
+    after   = load(fullfile(load_pth, datasets(after_dataset).name));
+   
          
     denoisedData = dir(fullfile(load_pth, sprintf('s0%d_denoisedData*',subject_num+1)));
     denoisedData = load(fullfile(load_pth,denoisedData(1).name));
     
     spectral_data_files  = dir(fullfile(load_pth, 'spectral_data*.mat'));
-    spectral_data_before = load(fullfile(load_pth,spectral_data_files(2).name));
-    spectral_data_after  = load(fullfile(load_pth,spectral_data_files(2).name));
+    
+    
+    for ii = 1:numel(spectral_data_files)
+        fprintf('%d: %s\n', ii, spectral_data_files(ii).name)
+    end
+    before_dataset = input('Before data set number?');
+    after_dataset = input('After data set number?');
+ 
+    spectral_data_before = load(fullfile(load_pth,spectral_data_files(before_dataset).name));
+    spectral_data_after  = load(fullfile(load_pth,spectral_data_files(after_dataset).name));
     
     
     model_fit_before = nanmedian(before.fit_f2,4);
@@ -69,16 +76,19 @@ for subject_num = which_data_to_visualize
    
     t = (1:1000)/1000;
     f = (0:length(t)-1)/max(t);
-%     figure('position', [1,600,1400,800]); clf;
-    for chan =  1:20
+    f_sel     = intersect(f, before.f_use4fit);
+    fH = figure('position', [1,600,1400,800]); clf;
+    for chan =  13;%1:20
+        set(fH, 'name', sprintf('Channel %d', chan));
         for ii = 1:9
-            figure(ii), clf;
+            %figure(ii), 
+            clf;
 %             subplot(10,1,ii)
         
-            plot(f,10.^model_fit_before(ii,:,chan),'color', color_scheme(ii,:,:), 'LineWidth',4); hold on;
-            plot(f,data_before(:,ii,chan), 'color', color_scheme(ii,:,:), 'LineWidth',2);
-            
-            plot(f,10.^model_fit_before(10,:,chan),'color',rgb_grey,'LineWidth',4);
+            plot(f(f_sel),10.^model_fit_before(ii,f_sel,chan),'color', color_scheme(ii,:,:), 'LineWidth',4); hold on;
+            %plot(f,data_before(:,ii,chan), 'color', color_scheme(ii,:,:), 'LineWidth',2);
+            plot(f,mean(data_before(:,5:8,chan),2), '-o', 'color', color_scheme(ii,:,:), 'LineWidth',2);
+            plot(f(f_sel),10.^model_fit_before(10,f_sel,chan),'color',rgb_grey,'LineWidth',4);
             plot(f,data_before(:,10,chan), 'color', rgb_grey, 'LineWidth',2);
 
             
@@ -91,13 +101,14 @@ for subject_num = which_data_to_visualize
             xlabel('Frequency (Hz)','FontSize',18)
             ylabel('Power','FontSize',18)
             legend(condition_names{ii}, 'Data', 'Baseline');
-            hgexport(gcf, fullfile(project_pth, subj_pths{subject_num}, 'figs',sprintf('data_modelfit_before_chan%d_cond%d_4',chan,ii)));
+            %hgexport(gcf, fullfile(project_pth, subj_pths{subject_num}, 'figs',sprintf('data_modelfit_before_chan%d_cond%d_4',chan,ii)));
+            waitforbuttonpress
         end
     
     end
     
     model_fit_after = nanmedian(after.fit_f2,4);
-    data_after     = nanmedian(spectral_data_after.spectral_data_boots,4);
+    data_after      = nanmedian(spectral_data_after.spectral_data_boots,4);
     
 %     data_after(29,:,:) = NaN;
 %     data_after(32,:,:) = NaN;
@@ -110,15 +121,16 @@ for subject_num = which_data_to_visualize
     
     t = (1:800)/1000;
     f = (0:length(t)-1)/max(t);
+    f_sel     = intersect(f, after.f_use4fit);
     for chan = 40:80
         for ii = 1:9
             figure(ii), clf;
 
 
-            plot(f,10.^model_fit_after(ii,:,chan),'color', color_scheme(ii,:,:), 'LineWidth',4); hold on;
+            plot(f(f_sel),10.^model_fit_after(ii,f_sel,chan),'color', color_scheme(ii,:,:), 'LineWidth',4); hold on;
             plot(f,data_after(:,ii,chan), 'color', color_scheme(ii,:,:), 'LineWidth',2);
             
-            plot(f,10.^model_fit_after(10,:,chan),'color',rgb_grey,'LineWidth',4);
+            plot(f(f_sel),10.^model_fit_after(10,f_sel,chan),'color',rgb_grey,'LineWidth',4);
             plot(f,data_after(:,10,chan), 'color', rgb_grey, 'LineWidth',2);
 
 %                         hgexport(gcf, fullfile(project_pth, subj_pths{subject_num}, 'figs',sprintf('data_modelfit_after_chan%d_allcond',chan,ii)));
