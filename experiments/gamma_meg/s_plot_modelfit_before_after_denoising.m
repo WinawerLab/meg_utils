96320f8457c7c4bde612d412b59096930590f502
Jonathan Winawer <elinekupers@gmail.com>
2015-10-07 10:47:41 -0400

    added notes to the spectra plotting script

--- experiments/gamma_meg/s_plot_modelfit_before_after_denoising.m
+++ experiments/gamma_meg/s_plot_modelfit_before_after_denoising.m
@@ -3,33 +3,28 @@
 
 % Note about the different datasets one can choose from:
 %                   Peak restr. Freq used
-% Modelfit 1:       35-80 Hz    f>=35 & f < 40 |(f > 40 & f <= 57) | (f>=65 & f <= 115) | (f>=126 & f <= 175) | (f>=186 & f <= 200));
-% Modelfit 2:       40-80 Hz    f>=35 & f <= 58) | (f>=62 & f <= 115) | (f>=126 & f <= 175) | (f>=186 & f <= 200));
-% Modelfit 3:       40-80 Hz    f>=35 & f <= 57) | (f>=63 & f <= 115) | (f>=126 & f <= 175) | (f>=186 & f <= 200));
-% Modelfit 4:       42-80 Hz    f>=35 & f <= 57) | (f>=63 & f <= 115) | (f>=126 & f <= 175) | (f>=186 & f <= 200));
-% Modelfit 5:       46-80 Hz    f>=35 & f <= 57) | (f>=63 & f <= 115) | (f>=126 & f <= 175) | (f>=186 & f <= 200));
+% Modelfit 1:       35-80 Hz    (f>=35 & f < 40)  | (f > 40 & f <= 57) | (f>=65 & f <= 115)  | (f>=126 & f <= 175) | (f>=186 & f <= 200));
+% Modelfit 2:       40-80 Hz    (f>=35 & f <= 58) | (f>=62 & f <= 115) | (f>=126 & f <= 175) | (f>=186 & f <= 200));
+% Modelfit 3:       40-80 Hz    (f>=35 & f <= 57) | (f>=63 & f <= 115) | (f>=126 & f <= 175) | (f>=186 & f <= 200));
+% Modelfit 4:       42-80 Hz    (f>=35 & f <= 57) | (f>=63 & f <= 115) | (f>=126 & f <= 175) | (f>=186 & f <= 200));
+% Modelfit 5:       46-80 Hz    (f>=35 & f <= 57) | (f>=63 & f <= 115) | (f>=126 & f <= 175) | (f>=186 & f <= 200));
 
-
+% Choose project path
 project_pth                   = '/Volumes/server/Projects/MEG/Gamma/Data';
 data_pth                      = '*_Gamma_*subj*';
 
-
+% Define parameters
 fs                            = 1000;
-intertrial_trigger_num        = 10;
 which_data_to_visualize       = 6;
 save_images                   = true;
 
-before_dataset                = 2;
-after_dataset                 = 4;
-
-% meg_add_fieldtrip_paths('/Volumes/server/Projects/MEG/code/fieldtrip',{'yokogawa', 'sqdproject'})
-
+% Get the folders of the subjects in the Gamma experiment
+% Note: the session numbers are always subject_nr +1 
 d = dir(fullfile(project_pth, data_pth));
 subj_pths = struct2cell(d);
 subj_pths = subj_pths(1,:);
 
-
-
+% Define colors
 rgb_pink  = [228,43,145]/255;
 rgb_green   = [141,200,108]/255;
 rgb_purple    = [93,12,139]/255;
@@ -38,37 +33,39 @@ rgb_grey    = [75, 75, 75]/255;
 color_scheme = [rgb_pink;rgb_pink;rgb_pink;rgb_pink; ...
                 rgb_green;rgb_green;rgb_green;rgb_green;rgb_purple];
 
+% Define saturated colors in case you want to plot all fits in one figure
 satValues       = 1-linspace(0.1,1,4);
 colorRGB_pink   = varysat(rgb_pink,satValues);
 colorRGB_noise  = varysat(rgb_green,satValues);
-
-% colorscond_sat = [colorRGB_pink,colorRGB_noise,rgb_purple];
-            
-            
+       
 %% Loop over data sets
-cmap = copper(9);
 for subject_num = which_data_to_visualize
     
+    % Get condition names (different for the first and last 6 subjects)
     condition_names  = gamma_get_condition_names(subject_num);
 
+    % Go to specific subject folder and find datasets
     load_pth    = fullfile(project_pth, subj_pths{subject_num}, 'processed');
     datasets    = dir(fullfile(load_pth, '*boot*'));
     
+    % Define which datasets you want to load
     for ii = 1:numel(datasets)
         fprintf('%d: %s\n', ii, datasets(ii).name)
     end
+    
+    % Before is without denoising, after is with denoising
     before_dataset = input('Before data set number?');
     after_dataset = input('After data set number?');
     
     before  = load(fullfile(load_pth, datasets(before_dataset).name));   
     after   = load(fullfile(load_pth, datasets(after_dataset).name));
    
-         
+    % Get denoised dataset in case we need to define the badChannels 
     denoisedData = dir(fullfile(load_pth, sprintf('s0%d_denoisedData*',subject_num+1)));
     denoisedData = load(fullfile(load_pth,denoisedData(1).name));
     
+    % Get spectra for before and after denoising
     spectral_data_files  = dir(fullfile(load_pth, 'spectral_data*.mat'));
-    
     
     for ii = 1:numel(spectral_data_files)
         fprintf('%d: %s\n', ii, spectral_data_files(ii).name)
@@ -79,14 +76,16 @@ for subject_num = which_data_to_visualize
     spectral_data_before = load(fullfile(load_pth,spectral_data_files(before_dataset).name));
     spectral_data_after  = load(fullfile(load_pth,spectral_data_files(after_dataset).name));
     
-    
+    % Take the median across bootstraps for modelfit and spectra
     model_fit_before = nanmedian(before.fit_f2,4);
     data_before     = nanmedian(spectral_data_before.spectral_data_boots,4);
     
-   
+    % Define time and frequencies to use
     t = (1:1000)/1000;
     f = (0:length(t)-1)/max(t);
     f_sel     = intersect(f, before.f_use4fit);
+    
+    % Plot
     fH = figure('position', [1,600,1400,800]); clf;
     for chan =  13;%1:20
         set(fH, 'name', sprintf('Channel %d', chan));
@@ -117,6 +116,8 @@ for subject_num = which_data_to_visualize
     
     end
     
+    
+    % Do the same but then for model and spectra after denoising
     model_fit_after = nanmedian(after.fit_f2,4);
     data_after      = nanmedian(spectral_data_after.spectral_data_boots,4);
     
@@ -129,6 +130,7 @@ for subject_num = which_data_to_visualize
 %     data_after(141,:,:) = NaN;
 %     data_after(150,:,:) = NaN;
     
+    % Epochs are now only 800 ms instead of 1000.
     t = (1:800)/1000;
     f = (0:length(t)-1)/max(t);
     f_sel     = intersect(f, after.f_use4fit);
