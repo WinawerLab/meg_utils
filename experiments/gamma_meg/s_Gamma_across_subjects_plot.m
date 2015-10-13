f4fe9bb086e2210700f482dcee68b48b86559be5
Jonathan Winawer <elinekupers@gmail.com>
2015-10-13 12:52:55 -0400

    visualize sessions updates

--- experiments/gamma_meg/s_Gamma_across_subjects_plot.m
+++ experiments/gamma_meg/s_Gamma_across_subjects_plot.m
@@ -3,16 +3,23 @@
 project_pth                   = '/Volumes/server/Projects/MEG/Gamma/Data';
 data_pth                      = '*_Gamma_*subj*';
 
+% Subjects in the binarized noise experiment are: [10:12,14:16]
+% Subjects in the non-binarized noise experiment are: [5:9]
+
+
 fs                            = 1000;
 intertrial_trigger_num        = 10;
-session_num                   = [9:11,13];
+session_nums                  = [5:9]; %[10:12,14:16];
 save_images                   = false;
 save_pth                      = fullfile(project_pth, 'Images');
-use_denoised_data             = true;
+use_denoised_data             = false;
 if use_denoised_data;
-     delete_me = 3; 
-else delete_me = 0; 
+     delete_me = 4; 
+else delete_me = 1; 
 end;
+
+
+suffix                         = 'NotBinarizedDenoised_localregression';
 
 addpath(genpath('/Volumes/server/Projects/MEG/code/fieldtrip'))
 
@@ -37,31 +44,34 @@ bad_channels = [];
 snr_w_pwr    = [];
 snr_w_gauss  = [];
 
-for subject_num = session_num
+scrsz = get(0,'ScreenSize');
+
+
+for session = session_nums
     
 
     
     %% Load Data
-    load_pth                = fullfile(project_pth, subj_pths{subject_num}, 'processed');
+    load_pth                = fullfile(project_pth, subj_pths{session -1}, 'processed');
     if use_denoised_data; d =  dir(fullfile(load_pth, '*denoisedData_bootstrapped100_2*'));  % Check whether d(1) or d(2) is used, for positive or both sides modelfit
-    else                  d =  dir(fullfile(load_pth, '*_bootstrappedData.mat')); end
+    else                  d =  dir(fullfile(load_pth, '*_localregression*100.mat')); end
         
-    AllResults{subject_num-delete_me} = load(fullfile(load_pth, d(1).name));
-    w_gauss                 = AllResults{subject_num -delete_me}.w_gauss;
-    w_pwr                   = AllResults{subject_num -delete_me}.w_pwr;
+    AllResults{session-delete_me} = load(fullfile(load_pth, d(1).name));
+    w_gauss                 = AllResults{session -delete_me}.w_gauss;
+    w_pwr                   = AllResults{session -delete_me}.w_pwr;
     
     % Load denoisedData to get bad channels.
     if use_denoised_data
         d                       =  dir(fullfile(load_pth, '*denoisedData.mat'));
         denoisedData            = load(fullfile(load_pth, d(1).name));
-        bad_channels{subject_num-delete_me} = denoisedData.bad_channels;
-    else bad_channels{subject_num} = zeros(1,157);
+        bad_channels{session-delete_me} = denoisedData.bad_channels;
+    else bad_channels{session-1} = zeros(1,157);
     end
     
     
     
-    num_conditions          = size(AllResults{subject_num-delete_me}.out_exp,2);
-    num_channels            = size(AllResults{subject_num-delete_me}.out_exp,1);
+    num_conditions          = size(AllResults{session-delete_me}.w_gauss,2);
+    num_channels            = size(AllResults{session-delete_me}.w_gauss,1);
     
     %% Calculating SNR contrasts
     
@@ -111,34 +121,34 @@ for subject_num = session_num
     tmp_data = permute(w_pwr, [2 1 3]);
     tmp_data = reshape(tmp_data, num_conditions, []);
     tmp = contrasts*tmp_data;
-    tmp = reshape(tmp, num_contrasts, num_channels, AllResults{subject_num-delete_me}.nboot);
-    snr_w_pwr{subject_num-delete_me} = summary_stat(tmp)';
+    tmp = reshape(tmp, num_contrasts, num_channels, AllResults{session-delete_me}.nboot);
+    snr_w_pwr{session-delete_me} = summary_stat(tmp)';
     
     tmp_data = permute(w_gauss, [2 1 3]);
     tmp_data = reshape(tmp_data, num_conditions, []);
     tmp = contrasts*tmp_data;
-    tmp = reshape(tmp, num_contrasts, num_channels, AllResults{subject_num-delete_me}.nboot);
-    snr_w_gauss{subject_num-delete_me}  = summary_stat(tmp)';
+    tmp = reshape(tmp, num_contrasts, num_channels, AllResults{session-delete_me}.nboot);
+    snr_w_gauss{session-delete_me}  = summary_stat(tmp)';
     
 end
 
 snr_w_gauss_157 = [];
 snr_w_pwr_157   = [];
 for ii = 1:num_contrasts
-    for jj = session_num-delete_me
+    for jj = session_nums-delete_me
         snr_w_gauss_157{ii,jj} = to157chan(snr_w_gauss{jj}(:,ii)',~bad_channels{jj}, 'nans');
         snr_w_pwr_157{ii,jj} = to157chan(snr_w_pwr{jj}(:,ii)',~bad_channels{jj}, 'nans');      
     end
 end
 
-snr_w_gauss_157 = reshape(catcell(1,snr_w_gauss_157),[num_contrasts,numel(session_num),157]);
-snr_w_pwr_157   = reshape(catcell(1,snr_w_pwr_157),[num_contrasts,numel(session_num),157]);
+snr_w_gauss_157 = reshape(catcell(1,snr_w_gauss_157),[num_contrasts,numel(session_nums),157]);
+snr_w_pwr_157   = reshape(catcell(1,snr_w_pwr_157),[num_contrasts,numel(session_nums),157]);
 
 
 %% SNR Mesh per stimulus type Gamma
 threshold = 0;%3;
 % gaussian weight for each stimuli
-fH = figure(1005); clf, set(fH, 'name', 'Gaussian weight' )
+fH = figure(1); clf, set(fH, 'name', 'Gaussian weight' ); set(fH, 'position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2]);
 
 for c = 1:9
     subplot(3,3,c)
@@ -153,14 +163,14 @@ data.snr_w_gauss   = snr_w_gauss;
 data.data_to_plot  = data_to_plot;
 data.contrastnames = {contrastnames{c}};
 
-hgexport(fH, fullfile(save_pth,'Mesh_stimulus_type_gamma_SNR_across_subjects_BinarizedDenoised_4'));
+hgexport(fH, fullfile(save_pth,sprintf('Mesh_stimulus_type_gamma_SNR_across_subjects_%s',suffix)));
 set(gcf, 'UserData', data);
-saveas(fH, fullfile(save_pth,'Mesh_stimulus_type_gamma_SNR_across_subjects_BinarizedDenoised_4'), 'fig');
+saveas(fH, fullfile(save_pth,sprintf('Mesh_stimulus_type_gamma_SNR_across_subjects_%s',suffix)), 'fig');
 
 %% SNR Mesh difference Noise and Gratings
 threshold = 0;%3;
-% gaussian weight for each stimuli
-fH = figure(455); clf, set(fH, 'name', 'Gaussian weight')
+% gaussian weight for across stimuli
+fH = figure(2); clf, set(fH, 'name', 'Gaussian weight'); set(fH, 'position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2]);
 %for c = 1:12
 for c = 10:13
     subplot(2,2,c-9)
@@ -175,21 +185,21 @@ data.snr_w_gauss   = snr_w_gauss;
 data.data_to_plot  = data_to_plot;
 data.contrastnames = {contrastnames{c}};
 
-hgexport(fH, fullfile(save_pth,'Mesh_difGratNoise_gamma_SNR_across_subjects_BinarizedDenoised_4'));
+hgexport(fH, fullfile(save_pth,sprintf('Mesh_difGratNoise_gamma_SNR_across_subjects_%s',suffix)));
 set(gcf, 'UserData', data)
-saveas(fH, fullfile(save_pth,'Mesh_difGratNoise_gamma_SNR_across_subjects_BinarizedDenoised_4'), 'fig');
+saveas(fH, fullfile(save_pth,sprintf('Mesh_difGratNoise_gamma_SNR_across_subjects_%s',suffix)), 'fig');
 
 %% SNR Mesh per stimulus type Broadband
 threshold = 0;%3;
-% gaussian weight for each stimuli
-fH = figure(1005); clf, set(fH, 'name', 'Broadband weight' )
+% broadband weight for each stimuli
+fH = figure(3); clf, set(fH, 'name', 'Broadband weight' ); set(fH, 'position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2]);
 
 for c = 1:9
     subplot(3,3,c)
     data_to_plot = squeeze(nanmean(snr_w_pwr_157(c,:,:),2))';
     data_to_plot(abs(data_to_plot) < threshold) = 0;
     ft_plotOnMesh(data_to_plot, contrastnames{c});
-    set(gca, 'CLim', [-1 1]* 2)
+    set(gca, 'CLim', [-1 1]* 1.5)
 end
 
 data = {};
@@ -198,20 +208,20 @@ data.data_to_plot  = data_to_plot;
 data.contrastnames = {contrastnames{c}};
 
 
-hgexport(fH, fullfile(save_pth,'Mesh_stimulus_type_broadband_SNR_across_subjects_BinarizedDenoised_4'));
-saveas(fH, fullfile(save_pth,'Mesh_stimulus_type_broadband_SNR_across_subjects_BinarizedDenoised_4'), 'fig');
+hgexport(fH, fullfile(save_pth,sprintf('Mesh_stimulus_type_broadband_SNR_across_subjects_%s',suffix)));
+saveas(fH, fullfile(save_pth,sprintf('Mesh_stimulus_type_broadband_SNR_across_subjects_%s',suffix)), 'fig');
 
 %% SNR Mesh
 threshold = 0;%3;
-% gaussian weight for each stimuli
-fH = figure(455); clf, set(fH, 'name', 'Broadband weight')
+% broadband weight for across stimuli
+fH = figure(4); clf, set(fH, 'name', 'Broadband weight'); set(fH, 'position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2]);
 %for c = 1:12
 for c = 10:13
     subplot(2,2,c-9)
     data_to_plot = squeeze(nanmean(snr_w_pwr_157(c,:,:),2))';
     data_to_plot(abs(data_to_plot) < threshold) = 0;
     ft_plotOnMesh(data_to_plot, contrastnames{c});
-    set(gca, 'CLim', [-1 1]* 2)
+    set(gca, 'CLim', [-1 1]* 1.5)
 end
 
 data = {};
@@ -219,6 +229,6 @@ data.snr_w_gauss   = snr_w_pwr;
 data.data_to_plot  = data_to_plot;
 data.contrastnames = {contrastnames{c}};
 
-hgexport(fH, fullfile(save_pth,'Mesh_difGratNoise_broadband_SNR_across_subjects_BinarizedDenoised_4'));
-saveas(fH, fullfile(save_pth,'Mesh_difGratNoise_broadband_SNR_across_subjects_BinarizedDenoised_4'), 'fig');
+hgexport(fH, fullfile(save_pth,sprintf('Mesh_difGratNoise_broadband_SNR_across_subjects_%s', suffix)));
+saveas(fH, fullfile(save_pth,sprintf('Mesh_difGratNoise_broadband_SNR_across_subjects_%s',suffix)), 'fig');
 
