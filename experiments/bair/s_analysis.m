4cae69e2efd983c3cf294f348269a4278dd26e30
elinekupers <elinekupers@gmail.com>
2017-08-17 15:54:45 -0400

    add some info in the comments about the experiment

--- experiments/bair/s_analysis.m
+++ experiments/bair/s_analysis.m
@@ -6,6 +6,46 @@
 % This script requires meg_utils and Fieldtrip
 
 
+
+% Info about the experiment:
+
+% HRF (zebra)
+%  Stimuli are presented for 0.25 seconds, with an exponentially
+%  distributed interstimulus interval (mean ~9s, range 3-24s).
+% HRF
+%       Exponential ISI, mean ~9s, range 3-24s
+% TASK
+%       12 s fixation task, 12 s relax, repeat 8 times (to measure anticipatory BOLD)
+% RETINOTOPY
+%       Like Dumoulin and Wandell, 2008: 8 sweeps ? 4 cardinal, 4 diagonal (diagonals include 50% blanks)
+% SPATIOTEMPORAL
+%        VISUAL: 36 unique stimuli, shown once each per scan (0.5 s except for temporal stimuli),
+%                with mean ISI of 4.5 s, range 3-6 s; orientation (3; 1 grating, 1 plaid, 1 circular);
+%                contrast (5; noise patterns); spacing: (5: noise patterns, 1 overlaps with contrast);
+%                objects (12: 3 faces, 3 scenes, 3 objects, 3 bodies); temporal (12; 6 durations; 6 ISIs);
+
+% SPATIO TEMPORAL (12 repeats, ECoG, fMRI; 24 for E/MEG !?)
+% For visual experiments, we use band-pass, gray-scale images, spanning
+% many stimulus dimensions. Twelve were used in a prior publication [69,70],
+% varying in contrast, number of component orientations (1, 2 or 16
+% superimposed gratings), or spacing between contrast elements (from very
+% sparse to very dense). Twelve are natural images of faces, objects, and
+% scenes (also gray-scale, band-pass). These stimuli will be presented for
+% 0.5 seconds each. Twelve other stimuli are simple noise patterns shown
+% with different temporal profiles (single pulses with variable duration;
+% or multiple pulses with variable interstimulus interval).
+
+
+% CRF      - 5 (zebra)                     KNK 162 164 166 168 116
+% Orient   - 3 (grating, plaid, circular)  KNK 150, 154, 158 (*HC)
+% Sparsity - 4 (zebras)                    KNK 181 182 183 184
+% 1 Pulse  - 6 (zebra??)                   KNK 183 * 6
+% 2 Pulses - 6 (zebra??)                   KNK 183 * 6
+
+% Faces -    4                             KNK 171 (sample 6 * 8 for 12 runs, 4 each)
+% Letters -  4                             KNK 173 (sample 6 * 8 for 12 runs, 4 each)
+% Scenes -   4                             KNK 175 (sample 6 * 8 for 12 runs, 4 each)
+
 %% 0. Define general parameters
 
 projectPth                    = '/Volumes/server/Projects/BAIR/MEG/data';
@@ -62,3 +102,8 @@ spectral_data_clean = abs(fft(tsClean))/length(t)*2;
 figure; plot(f,mean(spectral_data(:,:,1),2)); xlim([1 200]); set(gca, 'XScale', 'log', 'YScale', 'log')
 figure; plot(f,mean(spectral_data_clean(:,:,1),2)); xlim([1 200]); set(gca, 'XScale', 'log', 'YScale', 'log')
 
+%% Behavioral files
+
+d = dir(fullfile(projectPth, dataPth, 'behavior'));
+
+behavior_file = load(fullfile(projectPth, dataPth, 'behavior', d(3).name));
