bd354e25e687b6825bc5cd8e21edcead08c81cfd
elinekupers <elinekupers@gmail.com>
2018-11-09 18:26:19 -0500

    add header

--- tutorials/t_forwardmodels.m
+++ tutorials/t_forwardmodels.m
@@ -1,1 +1,19 @@
 %% t_forwardmodels
+
+% This is a tutorial to describe general steps to use MEG forward models
+% and predict responses from vertices on the surface to sensors
+%          
+%       https://osf.io/XXX
+
+% Overview:
+%   0. Download data from OSF, move to data folder and unzip
+%   1. 
+%   2. 
+%   3. 
+%   4. 
+
+% Dependencies
+%   * meg_utils (https://github.com/WinawerLab/meg_utils)
+%   * Brainstorm (https://github.com/brainstorm-3/)
+
+% see also: t_preprocessSampleData.m
